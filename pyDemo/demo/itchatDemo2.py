a15420bf85b3f468d7a4b7bce6cca1a91d8acf80
x1nL <845572410@qq.com>
2019-04-11 17:01:29 +0800

    修改微信图灵机器人BUG

--- pyDemo/demo/itchatDemo2.py
+++ pyDemo/demo/itchatDemo2.py
@@ -1,8 +1,10 @@
 import itchat
 import requests
 import json
-def get_response(msg)
-    api = r'http://openapi.tuling123.com/openapi/api/v2'# userid = int(time.time())
+
+def get_response(msg):
+    api = r'http://openapi.tuling123.com/openapi/api/v2'
+    # userid = int(time.time())
     data = {
         "perception": {
             "inputText": {
@@ -10,8 +12,8 @@ def get_response(msg)
             }
         },
         "userInfo": {
-            "apiKey": "6432af772fb641da95d79ef02fa5a2ca",
-            "userId": 'cba1a2bd794e513b',
+            "apiKey": "6432af772fb641da95d79ef02fa5a2ca1", #图灵机器人apikey
+            "userId": 'cba1a2bd794e513b', #随意字符串，但是图灵默认一个字符串为一个用户
         }
     }
     jsondata = json.dumps(data)
@@ -26,7 +28,7 @@ def print_content(msg):
 @itchat.msg_register([itchat.content.TEXT], isGroupChat=True) # 用于接收群里面的对话消息
 def print_content(msg):
     return get_response(msg['Text'])
-itchat.auto_login(hotReload==True) # 通过微信扫描二维码登录
+itchat.auto_login(hotReload=True) # 通过微信扫描二维码登录
 itchat.run()
 
 
